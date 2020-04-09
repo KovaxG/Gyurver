9dc84efe54ccfd9551410df243c9f0ab56178056
KovaxG <georgesmth202@gmail.com>
2020-04-10 00:54:53 +0300

    Serving Content/index.html by default

--- Main.hs
+++ Main.hs
@@ -8,4 +8,6 @@ main :: IO ()
 main = runServer (IP "localhost") (Port 8080) process
 
 process :: Request -> IO Response
-process _ = return $ mkResponse OK "<h1>Gyurver</h1><p>Hello, and welcome to my server :)</p>"
+process _ = do
+  contents <- readFile "Content/index.html"
+  return $ mkResponse OK contents
