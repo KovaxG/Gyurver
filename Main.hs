b46b3e141995e4bb2a4cceb2539368f42283b011
KovaxG <georgesmth202@gmail.com>
2020-04-10 00:57:28 +0300

    Added some putStrLns

--- Main.hs
+++ Main.hs
@@ -5,9 +5,12 @@ import Gyurver.Response
 import Gyurver.Server
  
 main :: IO ()
-main = runServer (IP "localhost") (Port 8080) process
+main = do
+  putStrLn "Gyurver is starting." 
+  runServer (IP "localhost") (Port 8080) process
 
 process :: Request -> IO Response
 process _ = do
+  putStrLn "Got some request, whatever. Imma just send Contents/index.html"
   contents <- readFile "Content/index.html"
   return $ mkResponse OK contents
