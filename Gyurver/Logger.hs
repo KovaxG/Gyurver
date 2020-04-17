ca10e93bcb0d049c9dc4d63b790ac2064ef5392a
KovaxG <georgesmth202@gmail.com>
2020-04-17 22:28:52 +0300

    Added timestamp to logger

--- Gyurver/Logger.hs
+++ Gyurver/Logger.hs
@@ -1,11 +1,27 @@
 module Gyurver.Logger (Logger(..), info, error) where
 
 import Prelude hiding (error)
+import Data.Time
 
 data Logger = Console
 
-info :: Logger -> String -> IO ()
-info Console msg = putStrLn $ "[INFO] " ++ msg
+data LogMode = Info | Error
 
+instance Show LogMode where
+  show mode = case mode of 
+    Info -> "INFO"
+    Error -> "Error"
+
+info :: Logger -> String -> IO ()
+info Console = genericLog Console Info
+  
 error :: Logger -> String -> IO ()
-error Console msg = putStrLn $ "[ERROR] " ++ msg
+error Console = genericLog Console Error
+  
+genericLog :: Logger -> LogMode -> String -> IO ()
+genericLog logger logMode message = do
+  time <- getZonedTime
+  putStrLn $ sbrackets (show logMode ++ " " ++ show time) ++ " " ++ message
+
+sbrackets :: String -> String
+sbrackets s = "[" ++ s ++ "]"
