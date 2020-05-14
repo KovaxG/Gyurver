f8c8ed1a9cda459c7219fdbea236d0e06461a65e
KovaxG <georgesmth202@gmail.com>
2020-05-14 23:16:34 +0300

    Added JSON decoder

--- Component/Json.hs
+++ Component/Json.hs
@@ -1,6 +1,10 @@
 module Component.Json where
 
+import Data.Bifunctor
 import Data.List (intersperse)
+import Text.Parsec
+
+import Utils
 
 data Json 
   = JsonNull
@@ -25,3 +29,67 @@ instance Show Json where
     where
       lineToString (label, value) = 
         show label ++ ":" ++ show value
+
+parseJson :: String -> Either String Json
+parseJson = first show . parse json ""
+  where
+    json = 
+      jsonNull 
+      <|> jsonBool 
+      <|> jsonNumber 
+      <|> jsonString 
+      <|> jsonArray 
+      <|> jsonObject
+    
+    jsonNull = string "null" $> JsonNull
+    
+    jsonBool = jsonTrue <|> jsonFalse
+    jsonTrue = string "true" $> JsonBool True
+    jsonFalse = string "false" $> JsonBool False
+    
+    jsonNumber = try jsonFloat <|> jsonInt
+    jsonInt = do
+      number <- read <$> many1 digit
+      return $ JsonNumber number
+    jsonFloat = do
+      number <- many1 digit
+      char '.'
+      rest <- many1 digit
+      return $ JsonNumber $ read $ number ++ "." ++ rest
+      
+    jsonString = do
+      str <- stringCharacters
+      return $ JsonString str
+    
+    stringCharacters = do
+      char '\"'
+      -- TODO not great, can't have quotes in strings :(
+      str <- many (noneOf "\"") 
+      char '\"'
+      return str
+    
+    -- TODO fails at "[ 1 ]" for some reason
+    -- the spaces between brackets don't work :(
+    jsonArray = do
+      char '['
+      spaces
+      elements <- sepBy json (spaces >> char ',' >> spaces)
+      spaces
+      char ']'
+      return $ JsonArray elements
+
+    jsonObject = do
+      char '{'
+      spaces
+      attrs <- sepBy jsonAttr (spaces >> char ',' >> spaces)
+      spaces
+      char '}'
+      return $ JsonObject attrs
+    jsonAttr = do
+      fieldName <- stringCharacters
+      spaces
+      char ':'
+      spaces
+      element <- json
+      return (fieldName, element)
+      
