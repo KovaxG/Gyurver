68b83fb3720241ca118a9459b53f7388017d8a14
KovaxG <georgesmth202@gmail.com>
2020-05-25 01:45:15 +0300

    Added release script and fixed settings

---
+++ makeRelease.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Remove previous release
+rm -r Release
+
+# Folder structure
+mkdir Release
+pushd Release
+mkdir Content
+mkdir Data
+popd
+
+# Move favicon
+cp Content/favicon.ico Release/Content/favicon.ico
+cp -r Content/pdfs Release/Content/pdfs 
+
+
+# Elm stuff
+pushd Frontend
+elm make src/Main.elm --output ../Release/Content/main.html
+popd
+
+# Haskell stuff
+ghc Main.hs
+mv ./Main Release/Gyurver
+
