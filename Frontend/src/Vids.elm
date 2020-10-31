d9f02c89043553039c0e133290509f42c4c7f36f
Kovács György <gyorgy.kovacs@metrosystems.net>
2020-10-31 19:19:09 +0200

    Added tag filter to videos

--- Frontend/src/Vids.elm
+++ Frontend/src/Vids.elm
@@ -13,21 +13,27 @@ import Time exposing (Month(..))
 import Http exposing (Error)
 import Settings
 import Json.Decode as Decode exposing (Decoder)
-
+import Set
 import Types.Video as Video exposing (Video)
+import Html.Events exposing (onClick)
+
+type TagType = SelectedTag | NormalTag
 
 type alias Model =
   { videos : List Video
   , titleFilter : String
+  , tagFilter : List String
   }
 
 type Msg
   = SetVideos (List Video)
   | TitleFilterChanged String
+  | AddTag String
+  | RemoveTag String
 
 init : (Model, Cmd Msg)
 init =
-  ( { videos = [], titleFilter = "" }  -- TODO maybe add a loading state?
+  ( { videos = [], titleFilter = "", tagFilter = [] }  -- TODO maybe add a loading state?
   , Http.get
     { url = Settings.path ++ "/api/vids"
     , expect = Http.expectJson toMessage (Decode.list Video.decode)
@@ -43,23 +49,36 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
   SetVideos vids -> ({ model | videos = vids }, Cmd.none)
   TitleFilterChanged s -> ({ model | titleFilter = s }, Cmd.none)
+  AddTag tag -> ({ model | tagFilter = addIfNotContains tag model.tagFilter }, Cmd.none)
+  RemoveTag tag -> ({ model | tagFilter = model.tagFilter |> List.filter (\t -> t /= tag) }, Cmd.none)
 
 view : Model -> Document Msg
 view model =
-  let filtered = List.filter (\vid -> String.contains model.titleFilter vid.title) model.videos
+  let searchTermMatches vid = String.contains model.titleFilter vid.title
+      noSelectedTags = List.isEmpty model.tagFilter
+      oneTagIsSelectedFromVid vid = List.any (\t -> List.member t model.tagFilter) vid.tags
+      filtered = List.filter (\vid -> searchTermMatches vid && (noSelectedTags || oneTagIsSelectedFromVid vid)) model.videos
   in
     { title = "Videos"
     , body =
       [ CDN.stylesheet
       , Grid.container []
-        (h1 [] [text "Videos"] :: searchSection model :: List.map videoToHtml filtered)
+        (h1 [] [text "Videos"] :: searchSection model filtered :: List.map (videoToHtml model) filtered)
       ]
     }
 
-searchSection : Model -> Html Msg
-searchSection model =
-  div
-    []
+searchSection : Model -> List Video -> Html Msg
+searchSection model filteredVideos =
+  div [] (titleField model :: tagSelection model :: infoField filteredVideos :: [])
+
+infoField : List Video -> Html Msg
+infoField vids =
+  let videos = vids |> List.length
+  in div [] [text <| "Showing " ++ String.fromInt videos ++ " videos"]
+
+titleField : Model -> Html Msg
+titleField model =
+  div []
     [ text "Search by Title: "
     , input
       [ placeholder "..."
@@ -68,8 +87,18 @@ searchSection model =
       ] []
     ]
 
-videoToHtml : Video -> Html Msg
-videoToHtml vid =
+tagSelection : Model -> Html Msg
+tagSelection model =
+  let availableTags = model.videos |> List.concatMap .tags |> Set.fromList |> Set.toList |> List.map (tagToBadge model)
+      availableTagSection = text "Tags: " :: availableTags
+      takenTagSection = if (List.isEmpty model.tagFilter) then [] else (text "Selected: " :: (List.map (tagToBadge model) model.tagFilter))
+  in div []
+      [ div [] availableTagSection
+      , div [] takenTagSection
+      ]
+
+videoToHtml : Model -> Video -> Html Msg
+videoToHtml model vid =
   [ Grid.row [] [Grid.col [] [h3 [] [text vid.title]]]
   , Grid.row []
     [ Grid.col [] [iframe [src vid.url] []]
@@ -86,10 +115,7 @@ videoToHtml vid =
         |> Maybe.withDefault "¯\\_(ツ)_/¯"
         |> text
       , br [] []
-      , div []
-        ( strong [] [text "Tags "]
-          :: List.map (\tag -> Badge.badgePrimary [Spacing.ml1] [text tag]) vid.tags
-        )
+      , div [] (strong [] [text "Tags "] :: List.map (tagToBadge model) vid.tags)
       ]
     ]
   , Grid.row []
@@ -100,3 +126,12 @@ videoToHtml vid =
     ]
   , br [] []
   ] |> div []
+
+tagToBadge : Model -> String -> Html Msg
+tagToBadge model tag =
+  if List.member tag model.tagFilter
+  then Badge.badgeWarning [Spacing.ml1, onClick (RemoveTag tag)] [text tag]
+  else Badge.badgePrimary [Spacing.ml1, onClick (AddTag tag)] [text tag]
+
+addIfNotContains : a -> List a -> List a
+addIfNotContains x xs = if List.member x xs then xs else xs ++ [x]
