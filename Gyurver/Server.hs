2ead81df1749026395634d836118b6cf0404eda8
Kovács György <gyorgy.kovacs@metrosystems.net>
2020-10-31 15:42:11 +0200

    Changed an error message to a warning

--- Gyurver/Server.hs
+++ Gyurver/Server.hs
@@ -1,15 +1,13 @@
 module Gyurver.Server (runServer, IP(..), Port(..)) where
 
-import Data.ByteString.Char8 (ByteString)
-import Data.Maybe (fromMaybe)
 import Network.Simple.TCP (serve, HostPreference(..), send, recv, Socket, SockAddr)
 
-import Gyurver.Gyurror
-import Gyurver.Request
-import Gyurver.Response
+import Gyurver.Gyurror (Gyurror(..))
+import Gyurver.Request (parseRequest, Request)
+import Gyurver.Response (makeResponse, toByteString, Response, Status(BadRequest))
 import Gyurver.Logger (Logger)
 import qualified Gyurver.Logger as Log
-import Utils
+import Utils (maybeToEither)
 
 type RequestProcessor = Request -> IO Response
 data IP = IP String deriving (Show)
@@ -20,7 +18,7 @@ runServer log (IP address) (Port port) processRequest =
   serve (Host address) (show port) (processConnection log processRequest)
 
 processConnection :: Logger -> RequestProcessor -> (Socket, SockAddr) -> IO ()
-processConnection log processRequest (connectionSocket, remoteAddr) = do
+processConnection log processRequest (connectionSocket, _) = do
   getMessage >>= either handleFailure (sendResponse . processRequest)
   where
     getMessage :: IO (Either Gyurror Request)
@@ -33,10 +31,10 @@ processConnection log processRequest (connectionSocket, remoteAddr) = do
       case error of
         FailedReceive -> do
           sendResponse (return receiveFailedResponse)
-          Log.error log "I got a connection, but did not receive any message!"
+          Log.warn log "I got a connection, but did not receive any message!"
         FailedParse msg -> do
           sendResponse (return parseFailedResponse)
-          Log.error log "Failed to parse!"
+          Log.error log $ "Failed to parse request with message: \"" ++ msg ++ "\""
 
     sendResponse :: IO Response -> IO ()
     sendResponse responseIO =
